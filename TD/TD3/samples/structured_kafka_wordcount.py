e20387b786c02c0e9852007309c98ac4582d412a
Minh H.N NGUYEN <hoangnhatminh.nguyen@sanofi.com>
2023-11-06 06:45:14 +0100

    structured_kafka_wordcount example

---
+++ TD/TD3/samples/structured_kafka_wordcount.py
@@ -0,0 +1,56 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("""
+        Usage: structured_kafka_wordcount.py <bootstrap-servers> <subscribe-type> <topics>
+        """, file=sys.stderr)
+        sys.exit(-1)
+
+    bootstrapServers = sys.argv[1]
+    #subscribeType should be "subscribe" in this exercise.
+    subscribeType = sys.argv[2]
+    topics = sys.argv[3]
+    
+    spark = SparkSession\
+        .builder\
+        .appName("StructuredKafkaWordCount")\
+        .getOrCreate()
+        
+    # Create DataSet representing the stream of input lines from kafka
+    lines = spark\
+        .readStream\
+        .format("kafka")\
+        .option("kafka.bootstrap.servers", bootstrapServers)\
+        .option("sasl_mechanism", 'SCRAM-SHA-256')\
+        .option("security_protocol", 'SASL_SSL')\
+        .option("sasl_plain_username", "your-username")\
+        .option("sasl_plain_password", "your-password")\
+        .option(subscribeType, topics)\
+        .load()\
+        .selectExpr("CAST(value AS STRING)")
+
+    # Split the lines into words
+    words = lines.select(
+        # explode turns each item in an array into a separate row
+        explode(
+            split(lines.value, ' ')
+        ).alias('word')
+    )
+
+    # Generate running word count
+    wordCounts = words.groupBy('word').count()
+
+    # Start running the query that prints the running counts to the console
+    query = wordCounts\
+        .writeStream\
+        .outputMode('complete')\
+        .format('console')\
+        .start()
+
+    query.awaitTermination()
